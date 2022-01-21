3baddbb248f0193d99d6a9be3ca237f933865429
Josh Junon <junon@oro.sh>
2022-01-21 20:23:59 +0100

    feat: add phony build targets (closes #29)

--- oro-build/oro-build.lua
+++ oro-build/oro-build.lua
@@ -26,6 +26,10 @@ local List = require 'internal.util.list'
 local unpack = require 'internal.util.unpack'
 local P = require 'internal.path'
 local keys = require 'internal.util.keys'
+local escapeall = require 'internal.globals.rule.escapeall'
+local Path = (require 'internal.path-factory').Path
+local isinstance = require 'internal.util.isinstance'
+local flat = require 'internal.util.flat'
 
 -- Read config variables from the command line
 local raw_config = {}
@@ -39,7 +43,7 @@ end
 -- Initialize build script environment
 io.stderr:write('(Re-)configuring project...\n')
 
--- Run main build script
+-- Create context and perform the build
 local ctx = make_context {
 	source_directory = P.dirname(Oro.absbuildscript),
 	build_directory = Oro.absbindir,
@@ -49,6 +53,8 @@ local ctx = make_context {
 
 ctx.root_module:dofile(Oro.absbuildscript)
 
+-- Notify user about unreferenced command line config
+-- values
 local unreferenced = Set(keys(raw_config))
 for k,v in pairs(ctx.referenced_config) do
 	unreferenced[k] = nil
@@ -61,15 +67,52 @@ if #unreferenced > 0 then
 	end
 end
 
--- Add default generation rule (so that any config files
--- are checked in order to re-config)
-ctx.ninja:add_rule('_oro_build_regenerator', {
-	command = { 'cd', P.currentdir(), '&&', 'env', '_ORO_BUILD_REGEN=1', Oro.buildscript, Oro.bindir, unpack(Oro.arg) },
-	description = { 'Reconfigure', Oro.bindir },
-	generator = '1'
-})
+-- Construct phony targets
+-- We do this here as opposed to in the context
+-- since (in theory) a single module might have
+-- multiple scripts, and thus there is no module
+-- 'finalization' step until right now.
+local allphonies = List()
+local testphonies = List()
+local rootphonies = {}
+
+for _, module in pairs(ctx.modules) do
+	local offsetpath = P.normalize(P.join('.', P.relpath(Oro.abssrcdir, module.root)))
+	local isroot = module == ctx.root_module
+
+	for name, v in pairs(module.exports) do
+		local label = escapeall(offsetpath .. ':' .. name)
+
+		local deps = List()
+
+		for v in flat{v} do
+			if isinstance(v, Path) then
+				deps[nil] = v
+			end
+		end
+
+		if #deps > 0 then
+			ctx.ninja:add_phony(label, deps)
+
+			if name == 'all'      then allphonies[nil] = label
+			elseif name == 'test' then testphonies[nil] = label
+			elseif isroot         then rootphonies[name] = label
+			end
+		end
+	end
+end
+
+-- Combine `all` and `test` phonies into a single root phony
+ctx.ninja:add_phony('all', allphonies)
+ctx.ninja:add_phony('test', testphonies)
+
+-- Hoist root module's phonies to 'naked' phonies
+for k,v in pairs(rootphonies) do
+	ctx.ninja:add_phony(k, {v})
+end
 
 
+-- Enumerate all configuration dependencies
 local build_offset = P.relpath(Oro.absbindir, Oro.abssrcdir)
 local config_deps = List{ P.join(build_offset, P.basename(Oro.buildscript)) }
 local function add_build_dep(srcpath)
@@ -82,6 +125,14 @@ add_build_dep(P.join(Oro.rootdir, 'oro-build.c'))
 for _, srcpath in ipairs(keys(ctx.modules)) do
 	add_build_dep(P.relpath(Oro.abssrcdir, srcpath))
 end
+
+-- Add default generation rule (so that any config files
+-- are checked in order to re-config)
+ctx.ninja:add_rule('_oro_build_regenerator', {
+	command = { 'cd', P.currentdir(), '&&', 'env', '_ORO_BUILD_REGEN=1', Oro.buildscript, Oro.bindir, unpack(Oro.arg) },
+	description = { 'Reconfigure', Oro.bindir },
+	generator = '1'
+})
 
 ctx.ninja:add_build('_oro_build_regenerator', {
 	out = 'build.ninja',
